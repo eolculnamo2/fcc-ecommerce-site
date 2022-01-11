193ce85d00d72714728efb08c3836f46d7064c78
cmkarako <33211829+cmkarako@users.noreply.github.com>
2022-01-11 13:56:34 -0600

    logout-user

--- back-end/djangoapp/accounts/urls.py
+++ back-end/djangoapp/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-	path(r'signup/', views.signup_view, name="signup")
+	path(r'signup/', views.signup_view, name="signup"),
+	path(r'login/', views.login_view, name="login"),
+	path(r'logout/', views.logout_view, name="logout"),
 ]
--- back-end/djangoapp/accounts/views.py
+++ back-end/djangoapp/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
@@ -8,9 +8,29 @@ def signup_view(request):
 		if request.method =='POST':
 			form = UserCreationForm(request.POST)
 			if form.is_valid():
-				form.save()
+				user = form.save()
 				# log the user in
+				login(request, user)
 				return redirect("articles:list")
 		else:
 			form = UserCreationForm()
 		return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+		if request.method == "POST":
+			form = AuthenticationForm(data=request.POST)
+			if form.is_valid():
+				user = form.get_user()
+				login(request, user)
+				return redirect("articles:list")
+
+		else:
+			form = AuthenticationForm()
+		return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+	if request.method == "POST":
+		logout(request)
+		return redirect("articles:list")
